795a97e9dc4bf42b49768cae5c608ee0a97f188e
J6N6S6 <jonasscandeias@gmail.com>
2025-03-19 15:09:35 -0300

    feat: add employee service

---
+++ src/main/java/com/jonas/project_compliance/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.jonas.project_compliance.service;
+
+import com.jonas.project_compliance.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+}
