5c4b0b9d25612de803cca29b56743f2cf4300f45
J6N6S6 <jonasscandeias@gmail.com>
2025-03-19 14:25:08 -0300

    First commit

---
+++ src/main/java/com/jonas/project_compliance/ProjectComplianceApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.project_compliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectComplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectComplianceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/project_compliance/ProjectComplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.project_compliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectComplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
