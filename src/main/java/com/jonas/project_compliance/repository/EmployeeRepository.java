62fa9b882f2a4f389d4f72d12cc6f96a2d8d283f
J6N6S6 <jonasscandeias@gmail.com>
2025-03-19 15:06:11 -0300

    feat: add employee repository

---
+++ src/main/java/com/jonas/project_compliance/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.jonas.project_compliance.repository;
+
+import com.jonas.project_compliance.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    
+}
