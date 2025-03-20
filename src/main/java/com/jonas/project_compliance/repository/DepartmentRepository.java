640a93a43d3af4943607bb49e726a98517d928f1
J6N6S6 <jonasscandeias@gmail.com>
2025-03-20 17:04:27 -0300

    feat: add department repository

--- src/main/java/com/jonas/project_compliance/repository/DepartmentRepository.java
+++ src/main/java/com/jonas/project_compliance/repository/DepartmentRepository.java
@@ -1,9 +1,9 @@
 package com.jonas.project_compliance.repository;
 
-import com.jonas.project_compliance.model.Employee;
+import com.jonas.project_compliance.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
