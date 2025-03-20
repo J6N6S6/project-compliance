6415c38437b4b42d129d4a89695ea8a6803b2443
J6N6S6 <jonasscandeias@gmail.com>
2025-03-20 17:08:35 -0300

    feat: add department service

---
+++ src/main/java/com/jonas/project_compliance/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.jonas.project_compliance.service;
+
+import com.jonas.project_compliance.repository.DepartmentRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    EntityManager entityManager;
+    
+}
