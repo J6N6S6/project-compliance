937f80371e4d218382be25174331fd61e29ed77b
J6N6S6 <jonasscandeias@gmail.com>
2025-03-20 23:36:06 -0300

    feat: add department controller create

--- src/main/java/com/jonas/project_compliance/controller/DepartmentController.java
+++ src/main/java/com/jonas/project_compliance/controller/DepartmentController.java
@@ -1,7 +1,12 @@
 package com.jonas.project_compliance.controller;
 
+import com.jonas.project_compliance.DTO.DepartmentDTO;
+import com.jonas.project_compliance.DTO.EmployeeDTO;
 import com.jonas.project_compliance.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,9 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> createDepartment(@RequestBody DepartmentDTO departmentDTO) {
+        DepartmentDTO department = departmentService.createDepartment(departmentDTO);
+        return ResponseEntity.ok(department);
+    }
 }
